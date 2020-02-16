6260f1df788c041b7aa790803b6ec0cf5afd1d4b
Francesco Negri <francesconegri@gmail.com>
2020-02-16 22:20:26 +0100

    Moar contents

--- main.js
+++ main.js
@@ -2,35 +2,136 @@
 const texts = {
   whatIs20plus20Title: {
     en: 'INFO & TIMETABLE',
-    pt: 'INFO & TIMETABLE'
+    pt: 'INFO & HORÁRIOS'
   },
   whatIs20plus20Description: {
-    en: `A small, unique music event, uniting artists and people from Italy, Portugal and elsewhere.\n\nProudly independent and DIY, by music fans for music fans. A new start (for a new decade). A different night for different times.\n\nTIMETABLE\n\nDoors: 19:00\nStart: 20:00\n\nStage times TBA`,
-    pt: `Um evento musical pequeno e único, unindo artistas e pessoas da Itália, Portugal e outros lugares.\n\nOrgulhosamente independente e DIY, por fãs de música para fãs de música. Um novo começo (para uma nova década). Uma noite diferente para tempos diferentes.\n\nTIMETABLE\n\nPortas: 19:00\nInício: 20:00\n\nStage times TBA`
+    en: `<strong>WHAT IS 20+20 FEST?</strong>
+
+A small, unique music event, uniting artists and people from Italy, Portugal and elsewhere.
+
+Proudly independent and DIY, by music fans for music fans. A new start (for a new decade). A different night for different times.
+
+
+<strong>LINKS</strong>
+
+<a href="https://www.facebook.com/20plus20/"><img class="social" src="img/fb.png" alt="Facebook"/></a> &nbsp;&nbsp;&nbsp; <a href="https://www.instagram.com/20plus20_fest/"><img class="social" src="img/ig.png" alt="Instagram"/></a>
+
+
+<strong>TIMETABLE</strong>
+
+Doors: 19:00
+Start: 20:00
+Stage times TBA
+`,
+    pt: `<strong>O QUE É 20+20 FEST</strong>
+
+Um evento musical pequeno e único, unindo artistas e pessoas da Itália, Portugal e outros lugares.
+
+Orgulhosamente independente e DIY, por fãs de música para fãs de música. Um novo começo (para uma nova década). Uma noite diferente para tempos diferentes.
+
+<strong>LINKS</strong>
+
+<a href="https://www.facebook.com/20plus20/"><img class="social" src="img/fb.png" alt="Facebook"/></a> &nbsp;&nbsp;&nbsp; <a href="https://www.instagram.com/20plus20_fest/"><img class="social" src="img/ig.png" alt="Instagram"/></a>
+
+<strong>HORÁRIOS</strong>
+
+Portas: 19:00
+Início: 20:00
+Stage times TBA
+`
   },
   lineUpTitle: {
     en: `ARTISTS`,
-    pt: `ARTISTS`
+    pt: `ARTISTAS`
   },
   lineUpDescription: {
-    en: `<a href="https://www.facebook.com/giardinidimiro/">Giardini di Mirò</a> (Italy) — Twenty years of career, more than a thousand concerts all over the world, Giardini di Mirò are a reference name in the Italian independent scene since the early 2000s. Their sound is a mix of psychedelia, post rock, electronic music, shoegaze, noise.\n\nMore artists to be announced`,
-    pt: `<a href="https://www.facebook.com/giardinidimiro/">Giardini di Mirò</a> (Itália) - Vinte anos de carreira, mais de mil shows em todo o mundo, Giardini di Mirò são um nome de referência no cenário independente italiano desde o início dos anos 2000. O som deles é uma mistura de psicodelia, pós-rock, música eletrônica, shoegaze, ruído.\n\nMais artistas a serem anunciados`
+    en: `<strong><a href="https://www.facebook.com/giardinidimiro/">GIARDINI DI MIRÒ</a> (Italy)</strong>
+
+<img src="img/gdm.jpg"/>
+
+Twenty years of career, more than a thousand concerts all over the world, Giardini di Mirò are a reference name in the Italian independent scene since the early 2000s. Their sound is a mix of psychedelia, post rock, electronic music, shoegaze, noise.
+
+<hr>
+
+<strong><a href="https://www.facebook.com/littlefriendsongs/">LITTLE FRIEND</a> (UK / Portugal)</strong>
+
+<img src="img/little_friend.jpg"/>
+
+Little Friend is the alter ego of John Almeida, an English-born musician and writer based in Porto, Portugal. Having in previous years played and fronted punk and rock-influenced bands, he chose to give a different direction to Little Friend’s sound, focusing on influences that encompass a wide array of musical sources, from 60s and 70s folk and singer-songwriter pop, to film soundtracks and country.
+
+<hr>
+
+<strong>MORE ARTISTS TO BE ANNOUNCED</strong>
+
+
+    `,
+    pt: `<strong><a href="https://www.facebook.com/giardinidimiro/">GIARDINI DI MIRÒ</a> (Itália)</strong>
+
+<img src="img/gdm.jpg"/>
+
+Vinte anos de carreira, mais de mil shows em todo o mundo, Giardini di Mirò são um nome de referência no cenário independente italiano desde o início dos anos 2000. O som deles é uma mistura de psicodelia, pós-rock, música eletrônica, shoegaze, ruído.
+
+<hr>
+
+<strong><a href="https://www.facebook.com/littlefriendsongs/">LITTLE FRIEND</a> (UK / Portugal)</strong>
+
+<img src="img/little_friend.jpg"/>
+
+<hr>
+
+<strong>MAIS ARTISTAS A SEREM ANUNCIADOS</strong>
+
+
+`
   },
   infoTicketsTitle: {
     en: `TICKETS & LOCATION`,
-    pt: `TICKETS & LOCATION`
+    pt: `BILHETES & LOCALIZAÇÃO`
   },
   infoTicketsDescription: {
-    en: 'Auditório CCOP\nRua do Duque de Loulé 202\n4000-324 Porto, Portugal\n\n\nTickets: €10 -- <a href="https://www.bol.pt/Comprar/Bilhetes/85980-20_20_fest-auditorio_ccop/">bol.pt</a>',
-    pt: 'Auditório CCOP\nRua do Duque de Loulé 202\n4000-324 Porto, Portugal\n\n\n',
+    en: `<strong>TICKETS</strong>
+
+€10
+
+<a href="https://www.bol.pt/Comprar/Bilhetes/85980-20_20_fest-auditorio_ccop/"><img class="small" src="img/bol.png"/></a>
+
+<hr>
+
+<strong>LOCATION</strong>
+
+    <strong><a href="https://www.facebook.com/auditorioccop/">Auditório CCOP</a></strong>
+
+Rua do Duque de Loulé 202
+4000-324 Porto, Portugal
+
+<a href="https://goo.gl/maps/S3XLcHbnCjubvDTg6"><img src="img/gmaps.png"/></a>
+
+`,
+    pt: `<strong>BILHETES</strong>
+
+€10
+
+<a href="https://www.bol.pt/Comprar/Bilhetes/85980-20_20_fest-auditorio_ccop/"><img class="small" src="img/bol.png"/></a>
+
+<hr>
+
+<strong>LOCALIZAÇÃO</strong>
+
+    <strong><a href="https://www.facebook.com/auditorioccop/">Auditório CCOP</a></strong>
+
+Rua do Duque de Loulé 202
+4000-324 Porto, Portugal
+
+<a href="https://goo.gl/maps/S3XLcHbnCjubvDTg6"><img src="img/gmaps.png"/></a>
+`,
   },
   navBarMessage: {
-    en: 'Saturday 21st March    +++    Auditório CCOP    +++    A small, unique music event    +++    Giardini di Mirò    +++    More artists to be announced    +++    ',
-    pt: 'Sábado 21 Março    +++    Auditório CCOP    +++    Um evento musical pequeno e único    +++    Giardini di Mirò    +++     Mais artistas a serem anunciados   +++    '
+    en: 'Saturday 21 March 2020    +++    Auditório CCOP    +++    A small, unique music event    +++    Giardini di Mirò    +++    Little Friend    +++    More artists to be announced    +++    ',
+    pt: 'Sábado 21 Março 2020    +++    Auditório CCOP    +++    Um evento musical pequeno e único    +++    Giardini di Mirò    +++    Little Friend    +++    Mais artistas a serem anunciados   +++    '
   },
   navBarMessage2: {
-    en: 'Saturday 21st March    +++    Auditório CCOP    +++    A small, unique music event    +++    Giardini di Mirò    +++    More artists to be announced    +++    ',
-    pt: 'Sábado 21 Março    +++    Auditório CCOP    +++    Um evento musical pequeno e único    +++    Giardini di Mirò    +++     Mais artistas a serem anunciados   +++    '
+    en: 'Saturday 21 March 2020    +++    Auditório CCOP    +++    A small, unique music event    +++    Giardini di Mirò    +++    Little Friend    +++    More artists to be announced    +++    ',
+    pt: 'Sábado 21 Março 2020    +++    Auditório CCOP    +++    Um evento musical pequeno e único    +++    Giardini di Mirò    +++    Little Friend    +++    Mais artistas a serem anunciados   +++    '
   },
   toggleLanguage: {
     en: '[en/pt]',
